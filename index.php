8388f18a599ae09207446773431c9358983d4fc3
jakenapay <jakemantesnapay@gmail.com>
2023-09-01 22:14:25 +0800

    Login UI

--- index.php
+++ index.php
@@ -2,10 +2,46 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Home</title>
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>"> <!-- There's a PHP code to remove the Cache -->
+    <link rel="stylesheet" href="assets/css/login.css?v=<?php echo time(); ?>">
+
 </head>
 <body>
-    
+
+    <!-- Beginning of the code -->
+    <div class="container">
+        <div class="row d-flex justify-content-center align-items-center vh-100">
+            <div class="col-md-5">
+                <div id="login-box">
+                    <img id="image-devcon" src="https://uploads-ssl.webflow.com/6492dd5d65d1855cb14a6692/6494571343918c14057fe090_DEVCON%20Kids%20Logo%20Horizontal.png" alt="DevCon Kids Image" loading="lazy">
+                    <p class="main-title">Inventory Management</p>
+                    <hr>
+                    <form action="">
+                        <!-- <p class="labels">Login</p> -->
+                        <p class="labels">Email Address</p>
+                        <input class="userInput" id="email" name="email" type="email">
+                        <p class="labels">Password</p>
+                        <input class="userInput" id="password" name="password" type="password">
+                        <button id="login-btn" class="button" name="login-btn" type="submit">login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
 </body>
 </html>
