01f2b44aa9bb895cf41b2d854b8ff11fadec1e91
morencloud <dinelajohnmoren@gmail.com>
2023-11-26 03:55:43 +0800

    working sa request item

---
+++ includes/getitem.inc.php
@@ -0,0 +1,24 @@
+<?php
+    include '../includes/config.inc.php';
+    if(isset($_POST['itemId'])){
+        $itemID = $_POST['itemId'];
+        $query = $pdo->prepare("SELECT * FROM items WHERE item_id = :itemID");
+         $query->bindParam(':itemID', $itemID, PDO::PARAM_INT); // Assuming item_id is an integer, adjust the data type accordingly
+
+    if ($query->execute()) {
+        $item = $query->fetch(PDO::FETCH_ASSOC);
+
+        if ($item) {
+            // Return the result as JSON
+            echo json_encode($item);
+        } else {
+            // Handle the case where the item with the provided item_id was not found
+            echo "error";
+        }
+    } else {
+        // Handle any database query execution errors
+        echo json_encode(['error' => 'Database error']);
+    }
+    }
+
+?>
--- nav.php
+++ nav.php
@@ -1,3 +1,5 @@
+
+
 <div class="section mainnav">
     <div class="row d-flex justify-content-center align-items-center">
         <div class="col-10">
@@ -25,16 +27,24 @@
             <nav>
                 <!-- Image logo -->
                 <div class="logo">
-                    <a href="home.php"><img src="https://uploads-ssl.webflow.com/6492dd5d65d1855cb14a6692/6494571343918c14057fe090_DEVCON%20Kids%20Logo%20Horizontal.png" alt="Logo" loading="lazy"></a>
+                    <a href="home.php"><img
+                            src="https://uploads-ssl.webflow.com/6492dd5d65d1855cb14a6692/6494571343918c14057fe090_DEVCON%20Kids%20Logo%20Horizontal.png"
+                            alt="Logo" loading="lazy"></a>
 
                 </div>
 
                 <!-- Links -->
                 <div class="links d-flex">
                     <ul>
-                        <li><a href="home.php" class="<?php echo (($_SESSION['active_tab'] === 'home.php')) ? 'active' : ''; ?>">Home</a></li>
-                        <li><a href="items.php" class="<?php echo (($_SESSION['active_tab'] === 'items.php')) ? 'active' : ''; ?>">Items</a></li>
-                        <li><a href="requests.php" class="<?php echo (($_SESSION['active_tab'] === 'requests.php')) ? 'active' : ''; ?>">Requests</a></li>
+                        <li><a href="home.php"
+                                class="<?php echo (($_SESSION['active_tab'] === 'home.php')) ? 'active' : ''; ?>">Home</a>
+                        </li>
+                        <li><a href="items.php"
+                                class="<?php echo (($_SESSION['active_tab'] === 'items.php')) ? 'active' : ''; ?>">Items</a>
+                        </li>
+                        <li><a href="requests.php"
+                                class="<?php echo (($_SESSION['active_tab'] === 'requests.php')) ? 'active' : ''; ?>">Requests</a>
+                        </li>
 
                         <!-- FOR CHAPTERS LINK -->
                         <!-- Only admins and manila chapter can see this page -->
@@ -44,24 +54,91 @@
                             echo '<li><a href="chapters.php" class="' . $activeClass . '">Chapters</a></li>';
                         }
                         ?>
-                        <li><a href="logs.php" class="<?php echo (($_SESSION['active_tab'] === 'logs.php')) ? 'active' : ''; ?>">Logs</a></li>
+                        <li><a href="logs.php"
+                                class="<?php echo (($_SESSION['active_tab'] === 'logs.php')) ? 'active' : ''; ?>">Logs</a>
+                        </li>
 
-                        <li><a href="cart.php" class="<?php echo (($_SESSION['active_tab'] === 'cart.php')) ? 'active' : ''; ?>"> <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <li>
+
+                        </li>
+
+
+
+                        <div class="dropdown show">
+                            <a class="btn btn-sm dropdown-toggle" href="" role="button" id="dropdownCart"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512">
+                                    <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                                     <style>
-                                        svg {
-                                            fill: #2a511f
-                                        }
+                                    svg {
+                                        fill: #2a511f
+                                    }
                                     </style>
-                                    <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                                    <path
+                                        d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
                                 </svg>
-                            </a><span class="badge " style="color: darkgreen"><?php echo $totalIDs ?></span></li>
+
+                                <span class="badge " style="color: darkgreen"><?php echo $totalIDs ?></span>
+                            </a>
+
+                            <div class="dropdown-menu" aria-labelledby="dropdownCart">
+                                <?php
+                                        try {
+                                            $stmt = $pdo->prepare("SELECT cart.*, items.*, users.* 
+                                            FROM cart
+                                            INNER JOIN items ON cart.item_id = items.item_id
+                                            INNER JOIN users ON cart.user_id = users.user_id");
+                                            $stmt->execute();
+                                            
+                                            // Fetch all rows as an associative array
+                                            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                                            // Process the result
+                                          foreach ($result as $row) {    ?>
+                                <div class="border-bottom" style="width: 250px; max-height: 400px; overflow-y: auto;">
+                                    <div class="row g-0">
+                                        <div class="col-md-6">
+                                            <img src="images\items\<?php echo $row['item_image'] ?>"
+                                                class="img-fluid rounded-start" style="width: 100%; max-height: 100%;"
+                                                alt="Item Image">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="card-body">
+                                                <h6 class="card-title"><?php echo $row['item_name'] ?></h6>
+                                                <p class="card-text"><?php echo $row['item_description'] ?></p>
+                                                <sub class="card-text">
+                                                    <small class="text-body-secondary">
+                                                        <?php
+                        $timestamp = strtotime($row['date_added']);
+                        $dateTime = new DateTime();
+                        $dateTime->setTimestamp($timestamp);
+                        $date = $dateTime->format('Y-m-d');
+                        echo "Date: $date";
+                        ?>
+                                                    </small>
+                                                </sub>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Your dropdown content goes here -->
+                                    <!-- Make sure this content is long enough to exceed 400px in height to see the scrollbar -->
+                                </div>
+
+
+                                <?php  }
+                                        } catch (PDOException $e) {
+                                            echo "Error: " . $e->getMessage();
+                                        }?>
+                            </div>
+                        </div>
                     </ul>
 
                     <ul>
                         <!-- <li><a href="profile.php" id="name"><?php echo $_SESSION['FN']; ?> <i class="fa-solid fa-caret-down"></i></a></li> -->
                         <!-- Dropdown -->
                         <div class="btn-group">
-                            <button type="button" id="btn-more-action" class="btn btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <button type="button" id="btn-more-action" class="btn btn-sm dropdown-toggle"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
                                 <?php
                                 // echo ($_SESSION['CT'] == 1) ? '<i class="fa-solid fa-user-tie"></i>' : '<i class="fa-solid fa-user"></i>';
@@ -74,7 +151,8 @@
                                 <a class="dropdown-item" href="faqs.php">FAQs</a>
                                 <div class="dropdown-divider"></div>
                                 <div class="">
-                                    <a class="dropdown-item logout" href="includes/logout.inc.php"><span>Log out</span><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+                                    <a class="dropdown-item logout" href="includes/logout.inc.php"><span>Log
+                                            out</span><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
                                 </div>
                             </div>
                         </div>
